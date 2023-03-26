0d6d36c44ed95d1a5f87172ef0f97c262efc72f9
jels <jels1991@gmail.com>
2023-03-26 12:34:46 -0400

    mejoras new

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { LlegandoComponent } from './components/llegando/llegando.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InicioComponent,
+    LlegandoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
